60448f57484d6aad3c43cf3d7f682416efa2bb31
yujung7768903 <yu-jung31476@naver.com>
2022-02-13 09:39:58 +0900

    Create toml config file

--- toml/config.py
+++ toml/config.py
@@ -5,22 +5,25 @@ from tomlkit import dump, load
 
 cfg_path = './config.toml'
 
+# 파일 수정하기
 def update_config():
     with open(cfg_path, 'r') as f:
         data = load(f)
     with open(f'{cfg_path}.bak', 'w') as f: # Create backup file
         dump(data, f)
-    data["agent"]["scaling-group"] = "HPC"
+    data["owner"]["name"] = "yujung"
     with open(cfg_path, 'w') as f:
         dump(data, f)
 
 update_config()
+
+
     
-config_data = toml.load("./config2.toml")
-config_data["agent"]["scaling-group"] = "HPC"
-f = open("./config2.toml", "w")
-toml.dump(config_data, f)
-f.close()
+# config_data = toml.load("./config2.toml")
+# config_data["agent"]["scaling-group"] = "HPC"
+# f = open("./config2.toml", "w")
+# toml.dump(config_data, f)
+# f.close()
 
 # doc = tomlkit.document()
 # doc["test"] = {"name": "test"}
