9801670cba66724cb89a22ba02a5bfb3d7cf76b3
yujung7768903 <yu-jung31476@naver.com>
2022-03-16 01:39:41 +0900

    Fix BJ_12738 time over error

--- algorithm_and_data_structure/search/BJ_12738_search.py
+++ algorithm_and_data_structure/search/BJ_12738_search.py
@@ -1,13 +1,37 @@
 import sys
-from collections import deque
 
 n = int(sys.stdin.readline())
 list_num = list(map(int, sys.stdin.readline().split()))
 
-deq = deque()
-max_lengh = 0
+# tartget보다 크거나 같은 수가 처음으로 나오는 인덱스
+def lower_bound(start, end, target): # 시작 인덱스, 끝 인덱스, 타겟
+    if start > end:
+        return start
 
-# 시간 초과
+    mid = (start + end) // 2 # 중간 인덱스
+    if answer[mid] == target:
+        return mid
+    elif answer[mid] < target:
+        return lower_bound(mid + 1, end, target)
+    else:
+        return lower_bound(start, mid - 1, target)
+    
+
+answer = []
+answer.append(list_num[0])
+for i in list_num[1:]:
+    if answer[-1] < i:
+        answer.append(i)
+    elif answer[-1] > i:
+        idx = lower_bound(0, len(answer) - 1, i)
+        answer[idx] = i
+
+print(len(answer))
+
+
+
+
+# 시간 초과된 방법
 # for i in range(1, n):
 #     print(f"=== i : {i} ===")
 #     deq_element = []
@@ -20,4 +44,4 @@ max_lengh = 0
 #         if max_lengh < len(deq_element):
 #             max_lengh = len(deq_element)
 
-print(max_lengh)
+# print(max_lengh)
