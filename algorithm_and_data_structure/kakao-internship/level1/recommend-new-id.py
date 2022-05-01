71238deeb8960b9c5b23dd95cecdf9fb3f815ce5
yujung7768903 <yu-jung31476@naver.com>
2022-05-01 23:46:30 +0900

    Modify recommend-new id and Solve menu-renewal problem

--- algorithm_and_data_structure/kakao-internship/level1/recommend-new-id.py
+++ algorithm_and_data_structure/kakao-internship/level1/recommend-new-id.py
@@ -1,17 +1,23 @@
 import re
 
 def solution(new_id):
+    # 1단계
     new_id = new_id.lower()
-    print(new_id)
+    # 2단계
     new_id = re.sub("[^a-z0-9\-_\.]", '', new_id)
+    # 3단계
     new_id = re.sub("\.\.+", '.', new_id)
+    # 4단계
     new_id = re.sub('^\.|\.$', '', new_id)
+    # 5단계
     if new_id == '':
         new_id = 'a'
+    # 6단계
     if len(new_id) >= 16:
         new_id = new_id[0:15]
     if new_id[-1] == '.':
         new_id = new_id[0:-1]
+    # 7단계
     while len(new_id) <= 2:
         new_id = new_id + new_id[-1]
     answer = new_id
